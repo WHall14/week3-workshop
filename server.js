b26765ca0d9de1b7974943670184a3c912a9acf7
Will <W.hall14@outlook.com>
2022-08-04 09:09:38 +1000

    Task 4 complete

---
+++ server.js
@@ -0,0 +1,49 @@
+//Task 2
+var express = require('express');
+var app = express();
+
+
+app.use(express.json());
+
+app.use(express.static(__dirname + '/www'));
+
+app.listen(3000, '127.0.0.1', function(){
+    var d = new Date();
+    var n = d.getHours();
+    var m = d.getMinutes();
+    console.log('Server has been started at : ' + n + ':' + m);
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/www/login.html');
+});
+
+app.get('/account', function (req, res) {
+    res.sendFile(__dirname + '/www/account.html');
+});
+
+
+//Task 3
+var users = [{email: 'john@john', password: 'doe'}, {email: 'jane@jane', password: 'doe'}, {email: 'will@will', password: 'hall'}];
+
+app.post('/api/login', function(req, res) {
+    if(!req.body){
+        return res.sendStatus(400);
+    }
+    console.log('Got body:', req.body);
+
+    //Creating a customer
+    var customer = {};
+    customer.email = req.body.email;
+    customer.upwd = req.body.upwd;
+    customer.valid = false;
+    
+    //Check if user exists with these credentials
+    for(let i=0; i < users.length; i++){
+        if(users[i].email == req.body.email && users[i].password == req.body.upwd){
+            customer.valid = true;
+        }
+    }
+    res.send(customer); 
+});
+
